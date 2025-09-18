7da7e6ff8e305b0f5c635ed65d7e174653acb2ed
Manuel costilla <santiagomanueldariocostilla@gmail.com>
2025-09-18 11:45:28 -0300

    vercel cambio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
+import vercel from "@astrojs/vercel/serverless"; // Importa el adaptador de Vercel
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: vercel(), // Agrega el adaptador aquí
   vite: {
     plugins: [tailwindcss()],
   },
