2cc12c5e5d1da067aa568f7d4cdddee4f1a0f50b
Manuel costilla <santiagomanueldariocostilla@gmail.com>
2025-09-25 12:02:11 -0300

    Co-authored-by: manuelcostilla <manuelcostilla@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default defineConfig({
   adapter: vercel(),
